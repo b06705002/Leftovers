d4368d00f600bcde345534a86c08c55afd37c950
Starshunter <petercheng0303@gmail.com>
2021-04-08 16:07:37 +0800

    Create basic HTML skeleton for StoreSetting.js

--- frontend/src/Page/Store/StoreSetting.js
+++ frontend/src/Page/Store/StoreSetting.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import "../../Styles/StoreSetting.css";
 
 class StoreSetting extends Component {
     /*
     This is StoreSetting Page, should implement:
-        1. Purpose (text)
-        2. Guiadance (text)
-        3. Developer intro (text)
-        4. Sing in (feature)
-            mail, password
-        5. Sign up (feature)
-            5.1 Store sign up
-            5.2 Customer sign up
     */
     render() {
         return (
-            <div>
+            <div className="Container">
                 <h1>StoreSetting Page</h1>
+                <div className="formContainer">
+                    <form className="formBlock">
+                        <label>Store Name</label>
+                        <input type="text" />
+                    </form>
+                    <form className="formBlock">
+                        <label>Phone</label>
+                        <input type="text" />
+                    </form>
+                    <form className="formBlock">
+                        <label>Mail Address</label>
+                        <input type="text" />
+                    </form>
+                    <form className="formBlock">
+                        <label>Address</label>
+                        <input type="text" />
+                    </form>
+                    <form className="formBlock">
+                        <label>Other</label>
+                        <input type="text" />
+                    </form>
+                </div>
             </div>
         );
     }
