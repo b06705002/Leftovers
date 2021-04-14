3a9690a6b8b83b3f0fb2accaac7eb469c0fb0eec
starshunter <petercheng0303@gmail.com>
2021-04-14 19:38:33 +0800

    Change api.py for testing

--- frontend/api/api.py
+++ frontend/api/api.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+
+@app.route('/api', methods=['POST'])
 def index():
-    return {
-        'name': "Hi"
-    }
+
+    mail = request.json['mail']
+    pwd = request.json['pwd']
+    msg = {'mail': mail, 'pwd': pwd, 'msg': 'you had login'}
+    return jsonify(msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# print(1+1)
