cf35983b6d527f6a18badc1d9fd567e9842b52fb
starshunter <petercheng0303@gmail.com>
2021-05-01 00:02:44 +0800

    Remove carousel controller in Login.js

--- frontend/src/Page/Login.js
+++ frontend/src/Page/Login.js
@@ -340,14 +340,14 @@ class Login extends Component {
                         </div>
 
                          {/* what are these ?*/}
-                        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        {/* <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span className="sr-only">Previous</span>
                         </a>
                         <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                             <span className="carousel-control-next-icon" aria-hidden="true"></span>
                             <span className="sr-only">Next</span>
-                        </a>
+                        </a> */}
                     </div>
                     <div style={{display: 'none'}}>
                         <input
