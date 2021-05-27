bb0ed51139111228b746b9e94d272ed865e2bced
b06705002 <b06705002@ntu.edu.tw>
2021-05-27 21:42:29 +0800

    <fix> duetime

--- frontend/api/api.py
+++ frontend/api/api.py
@@ -3,30 +3,12 @@ from pymongo import MongoClient
 import json
 import datetime
 from bson import ObjectId
-import atexit
-from apscheduler.scheduler import Scheduler
 
 app = Flask(__name__)
 
 # DB config
 client = MongoClient('mongodb+srv://SDMproject:SDMGROUP2@cluster0.w0fzh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db_name = 'SDM_project'
-
-# delete due time 
-cron = Scheduler(daemon=True)
-cron.start()
-
-@cron.interval_schedule(minutes=1)
-def job_function():
-    
-    db = client.get_database(db_name)
-    goods = db.Goods
-    for document in goods.find():
-        today = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+8)))
-        if today.strftime('%H:%M') == document['due']:
-            goods.delete_one({'_id': document.get('_id')})
-
-atexit.register(lambda: cron.shutdown(wait=False))
 
 # store
 @app.route('/api/store/login', methods=['POST'])
