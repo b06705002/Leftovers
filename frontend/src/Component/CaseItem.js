a733dffedc1a7807fe480060a99a4740089d1c1d
starshunter <petercheng0303@gmail.com>
2021-05-29 11:30:54 +0800

    Add user's mail to store's caseItem.

--- frontend/src/Component/CaseItem.js
+++ frontend/src/Component/CaseItem.js
@@ -9,9 +9,20 @@ class CaseItem extends Component {
             <li className={this.props.caseInfo.class} onClick={this.props.caseInfo.onClick} ref={this.props.caseInfo.ref}>
                 <h3>{this.props.caseInfo.store}</h3>
                 <p>{this.props.caseInfo.item}</p>
-                <p>數量{this.props.caseInfo.amount}</p>
-                <p>價錢{this.props.caseInfo.price}</p>
-                <p>地址{this.props.caseInfo.address}</p>
+                <p>數量 {this.props.caseInfo.amount}</p>
+                <p>價錢 {this.props.caseInfo.price}</p>
+                {/* address is not null mean it's user browsing */}
+                {this.props.caseInfo.address ? 
+                    <p>地址 {this.props.caseInfo.address}</p>
+                    :
+                    <></>
+                }
+                {/* address is not null mean it's user browsing */}
+                {!this.props.caseInfo.address ?
+                    <p>電子郵件 {this.props.caseInfo.mail}</p>
+                    :
+                    <></>
+                }
                 <img src={this.props.caseInfo.pic} width="100" height="100"></img>
                 {this.props.browse ? 
                     <>
